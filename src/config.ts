65a79aafce280e3e51dad117b354e12e3c7d8d42
yoss <yossthedev@gmail.com>
2023-08-22 08:55:13 -0400

    Change Page Description

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Y O S S | The Dev";
-export const SITE_DESCRIPTION =
-  "Desarrollador Front-End 🌐 y 🐧 Linuxero. Aprendo y escribo en mi Blog";
+export const SITE_TITLE = "Y O S S";
+export const SITE_DESCRIPTION = "Desarrollador 🌐 Front-End y 🐧 Linuxero";
