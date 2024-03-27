7d0e8a4247b436a8961bdb13cf0017575888c970
Yoannis Sánchez Soto <yossthedev@gmail.com>
2024-03-27 08:58:32 -0400

    update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import prefetch from "@astrojs/prefetch";
 import partytown from "@astrojs/partytown";
-import image from "@astrojs/image";
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,10 +15,5 @@ export default defineConfig({
     sitemap(),
     partytown(),
     tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-      cacheDir: "./.cache/image",
-      logLevel: "debug",
-    }),
   ],
 });
